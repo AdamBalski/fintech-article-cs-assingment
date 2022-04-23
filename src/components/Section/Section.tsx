e13b8f5ec97ef9a5893d5a642da9bb60d9f81bd5
AdamBalski <adambalski1@gmail.com>
2022-04-23 21:46:40 +0200

    ADDED: Images and authors info

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useContext} from "react";
 import "./Section.css";
+import {ThemeContext} from "../../ThemeContext";
 
 interface SectionEntity {
   order: number,
@@ -14,8 +15,11 @@ interface SectionProps {
 }
 
 function Section(props: SectionProps) {
+  const themeContext: any = useContext(ThemeContext);
+  const themeSuffix = themeContext.isDarkMode ? "-dark" : "-light";
+
   return (
-    <div className="Section">
+    <div className={`Section Section${themeSuffix}`}>
       <h1 style={{fontSize: props.section["title-font-size"]}}>{props.section.title}</h1>
       <span dangerouslySetInnerHTML={{__html: props.section.text}} style={{fontSize: props.section["title-font-size"]}}/>
     </div>
